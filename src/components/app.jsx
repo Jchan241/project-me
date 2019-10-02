52055219fbec48df39d1edfb97a539b49ee4c651
Johnny Chan <johchan24@gmail.com>
2019-10-03 08:44:14 +1000

    feature footer

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 
 import Banner from './banner';
 import Intro from './intro';
+import Footer from './footer';
+
 
 const App = () => {
   return (
     <div className="app">
       <Banner />
       <Intro />
+      <Footer />
     </div>
   );
 };
